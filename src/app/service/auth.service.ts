04d2af106d7006065f6584d88e0a1f654d969042
WillianX47 <willianx47@gmail.com>
2021-11-05 09:30:11 -0300

    [Feature Will - Task: Menu/Rodape logado | 04.11.21]

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,22 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { UserLogin } from '../model/UserLogin';
 import { Usuario } from '../model/Usuario';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  entrar(userlogin: UserLogin) : Observable<UserLogin>  {
-    return this.http.post<UserLogin>("https://willsblog.herokuapp.com/api/v1/usuario/logar", userlogin)
+  entrar(userlogin: UserLogin): Observable<UserLogin> {
+    return this.http.post<UserLogin>(
+      'https://willsblog.herokuapp.com/api/v1/usuario/logar',
+      userlogin
+    );
   }
 
-  cadastrar(usuario: Usuario) : Observable <Usuario> {
-    return this.http.post<Usuario>("https://willsblog.herokuapp.com/api/v1/usuario/cadastrar", usuario)
+  cadastrar(usuario: Usuario): Observable<Usuario> {
+    return this.http.post<Usuario>(
+      'https://willsblog.herokuapp.com/api/v1/usuario/cadastrar',
+      usuario
+    );
   }
 
+  logado() {
+    let ok: boolean = false;
+
+    if (environment.token != '') {
+      ok = true;
+    }
+    return ok;
+  }
 }
